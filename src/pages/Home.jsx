99e67894aa59998571d4152391f9071dbfe7aa3b
Cyril Rafalovich <replanttrees@gmail.com>
2025-06-13 22:39:03 -1000

    Whoopsie, did navbar instead of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Home(){
     return(
-        <></>
+        <>
+            <h1>Welcome to this puzzle</h1>
+            <h3>Try to figure out the password from the puzzle pieces.</h3>
+        </>
     )
 }
 
